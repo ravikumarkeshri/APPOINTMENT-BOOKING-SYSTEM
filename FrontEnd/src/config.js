2598d9cca2e958b9646c0a1e12e0f696f8701319
Ravi Kumar <136506172+ravikumarkeshri@users.noreply.github.com>
2024-04-19 19:25:37 +0530

    Update config.js

--- FrontEnd/src/config.js
+++ FrontEnd/src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:4500/api/v1"
-// export const BASE_URL = "https://appointment-booking-system-ten.vercel.app/"
+//export const BASE_URL = "http://localhost:4500/api/v1"
+ export const BASE_URL = "https://appointment-booking-system-theta.vercel.app//"
 
 export const token = localStorage.getItem('token') || sessionStorage.getItem('token'); 
