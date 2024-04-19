0505d87075886140194c3e1b5db1cd90e9f45059
ravikumarkeshri <studentwaledevelopers@gmail.com>
2024-04-19 21:42:21 +0530

    update

--- FrontEnd/src/config.js
+++ FrontEnd/src/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:4500/api/v1"
-export const BASE_URL = "https://appointment-booking-system-dusky.vercel.app/"
+export const BASE_URL = "https://appointment-booking-system-4.onrender.com"
 
 export const token = localStorage.getItem('token') || sessionStorage.getItem('token'); 
