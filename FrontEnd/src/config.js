b783f8d5a73091fc16aec3012ec2f15af3b38ddc
ravikumarkeshri <studentwaledevelopers@gmail.com>
2024-04-19 21:49:18 +0530

    update

--- FrontEnd/src/config.js
+++ FrontEnd/src/config.js
@@ -1,4 +1,4 @@
 // export const BASE_URL = "http://localhost:4500/api/v1"
-export const BASE_URL = "https://appointment-booking-system-4.onrender.com"
+export const BASE_URL = "https://appointment-booking-system-4.onrender.com/api/v1"
 
 export const token = localStorage.getItem('token') || sessionStorage.getItem('token'); 
