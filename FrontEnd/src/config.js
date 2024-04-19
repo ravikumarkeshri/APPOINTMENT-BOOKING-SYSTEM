7c5bc09d915a42209f7bcf001fe6604b2d3157f8
ravikumarkeshri <studentwaledevelopers@gmail.com>
2024-04-19 15:28:28 +0530

    update

--- FrontEnd/src/config.js
+++ FrontEnd/src/config.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:4500/api/v1"
+// export const BASE_URL = "http://localhost:4500/api/v1"
+export const BASE_URL = appointment-booking-system-zkv7.vercel.app
+
 export const token = localStorage.getItem('token') || sessionStorage.getItem('token'); 
